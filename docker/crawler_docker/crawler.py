8edcb944f55b364b1d8d964b7371a4b4f38bd1ff
JL-intech <j.limousin@intech-sud.fr>
2020-05-13 16:11:09 +0200

    Downloading function moved to crawler.py.

--- docker/crawler_docker/crawler.py
+++ docker/crawler_docker/crawler.py
@@ -6,6 +6,88 @@ import sys
 import os
 import json
 import time
+from git import Repo
+import tempfile
+import uuid
+import magic
+
+
+tmpDirectory="/sharedTmp"
+
+mime = magic.Magic(mime=True)
+
+
+def get_random_unique_filename(dest):
+    nameavail=False
+    if not os.path.isdir(dest):
+        return None
+    while not nameavail:
+        filename=uuid.uuid4().hex+'.txt'
+        if not os.path.exists(os.path.join(dest,filename)):
+            return filename
+
+
+########################################## FUNCTONS ##########################################
+
+#Get the repo name from the URL (by trimming)
+def get_repo_name_from_url(url):
+    last_slash_index = url.rfind("/")
+    last_suffix_index = url.rfind(".git")
+    if last_suffix_index < 0:
+        last_suffix_index = len(url)
+
+    if last_slash_index < 0 or last_suffix_index <= last_slash_index:
+        raise Exception("Badly formatted url {}".format(url))
+
+    return url[last_slash_index + 1:last_suffix_index]
+
+#========================================================================
+#MODULE DELCARATION AND CONFIGURATION SECTION
+
+def parser_pastebin(url, dest):
+    filename=get_random_unique_filename(dest)
+    with open(os.path.join(dest,filename), 'w') as tmp:
+        #fetch url and write content in temporary file
+        tmp.write(requests.get(url).text)
+        print("Compiled one pastebin file")
+    return [filename]
+
+def parser_github(url, dest):
+    
+    returns=[]
+    #get repo name
+    reponame=get_repo_name_from_url(url)
+    repopath=os.path.join(os.getcwd(), os.path.join(dest,reponame))
+    #clone the repo
+    if os.path.isdir(repopath):
+        repo = Repo(repopath)
+        pullinfo = repo.remotes.origin.pull()
+    #    pullchanges=repo.index.diff(pullinfo[0].old_commit) #TODO: check this
+
+    else:
+        repo = Repo.clone_from(url, repopath)
+    #find all files in the repo
+    for root, subdirs, files in os.walk(repo.working_tree_dir):
+        for file in files:
+            filepath=os.path.join(root, file)
+            if mime.from_file(filepath) == "text/plain": #only allow plain text files
+                #with open(filepath, "rb") as readfile:
+                    #append their content to the temporary file
+                    #shutil.copyfileobj(readfile, tmp)
+                returns.append(filepath)
+        
+    #send the temporary file file descriptor
+
+    return returns
+
+#add your crawler in this dict to register it, the key is the one the DB should use
+pre_parsers = {
+  "pastebin": parser_pastebin,
+  "git": parser_github,
+  "github": parser_github
+}
+
+
 #========================================================================
 #MODULE DELCARATION AND CONFIGURATION SECTION
 
@@ -53,61 +135,75 @@ crawlers = {
 
 #connecting to rabbitMQ
 
-success=False
-while not success:
-    try:
-        #connecting to mariadb
-        mariadb_connection = mariadb.connect(host='db_dict', user=os.environ['MYSQL_USER'], password=os.environ['MYSQL_PASSWORD'], database='crack_it')
-        success=True
-    except mariadb._exceptions.OperationalError as e:
-        success=False
-        print("Failed to connect to database... Retrying in 5 seconds.")
-        time.sleep(5)
+def main():
+    success=False
+    while not success:
+        try:
+            #connecting to mariadb
+            mariadb_connection = mariadb.connect(host='db_dict', user=os.environ['MYSQL_USER'], password=os.environ['MYSQL_PASSWORD'], database='crack_it')
+            success=True
+        except mariadb._exceptions.OperationalError as e:
+            success=False
+            print("Failed to connect to database... Retrying in 5 seconds.")
+            time.sleep(5)
 
-success=False
-while not success:
-    try:
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
-        channel = connection.channel()
-        success=True
-    except (pika.exceptions.AMQPConnectionError) as e:
-        success=False
-        print("Failed to connect to rabbitMQ ... Retrying in 5 seconds.")
-        time.sleep(5)
-
-
-#create the exchange if it does not exist already
-channel.exchange_declare(exchange='urls', exchange_type='fanout')
-
-result = channel.queue_declare(queue='parser_urls_queue', exclusive=False, auto_delete=False)
-queue_name = result.method.queue
-
-#bind the queue to the url exchange
-channel.queue_bind(exchange='urls', queue=queue_name)
+    success=False
+    while not success:
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+            channel = connection.channel()
+            success=True
+        except (pika.exceptions.AMQPConnectionError) as e:
+            success=False
+            print("Failed to connect to rabbitMQ ... Retrying in 5 seconds.")
+            time.sleep(5)
 
 
-#========================================================================
+    #create the exchange if it does not exist already
+    channel.exchange_declare(exchange='files', exchange_type='fanout')
 
-#QUERY INIT
-cursor = mariadb_connection.cursor()
-_SQL = (""" 
-        SELECT * FROM source
-        """)
-#QUERY EXECUTE
-cursor.execute(_SQL)
-result = cursor.fetchall()
+    result = channel.queue_declare(queue='parser_files_queue', exclusive=False, auto_delete=False)
+    queue_name = result.method.queue
 
-for row in result:
-    #We call the module the row is aksing for (value: row[2]) in the crawler dict, which is a registry of all modules. We then pass it the url from the DB row 
-    #the result is an array of urls
-    result, newsourcehint =crawlers[row[2]](row[1], row[4])
-    cursor.execute("UPDATE source SET sourceHint = %s WHERE idsource = %s;", (newsourcehint, row[0]))
-    mariadb_connection.commit() # w ecoult batch commit, but is it really worth it here ?
-    for value in result:
-        #assemble a json message to easely combine the two values, m=> module to use, v => url
-        message=json.dumps({"m": row[2], "v": value})
-        #send the message through rabbbitMQ using the urls exchange
-        channel.basic_publish(exchange='urls', routing_key='', body=message)
+    #bind the queue to the url exchange
+    channel.queue_bind(exchange='files', queue=queue_name)
 
-#closing connection to rabbitMQ
-connection.close()
+
+    #========================================================================
+
+    #QUERY INIT
+    cursor = mariadb_connection.cursor()
+    _SQL = (""" 
+            SELECT * FROM source
+            """)
+    #QUERY EXECUTE
+    cursor.execute(_SQL)
+    result = cursor.fetchall()
+
+    for row in result:
+        #We call the module the row is aksing for (value: row[2]) in the crawler dict, which is a registry of all modules. We then pass it the url from the DB row 
+        #the result is an array of urls
+        result, newsourcehint =crawlers[row[2]](row[1], row[4])
+        cursor.execute("UPDATE source SET sourceHint = %s WHERE idsource = %s;", (newsourcehint, row[0]))
+        mariadb_connection.commit() # w ecoult batch commit, but is it really worth it here ?
+        for itemToParse in result:
+            #assemble a json message to easely combine the two values, m=> module to use, v => url
+            datafiles=pre_parsers[row[2]](itemToParse, tmpDirectory)
+            for datafile in datafiles:
+
+                
+                #filedest=
+
+
+
+                message=json.dumps({"m": row[2], "s": itemToParse, "v": datafile})
+                #send the message through rabbbitMQ using the urls exchange
+                channel.basic_publish(exchange='files', routing_key='', body=message)
+
+
+
+    #closing connection to rabbitMQ
+    connection.close()
+
+if __name__ == '__main__':
+    main()
--- docker/parser_docker/parser.py
+++ docker/parser_docker/parser.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-import requests
 import MySQLdb as mariadb
 import pika
 import sys
 import json
-import tempfile
 import re as regex
 import hashid
-from git import Repo
 import os
 #import shutil
-import magic
 import time
 from zxcvbn import zxcvbn
+
+tmpDirectory="/sharedTmp"
 
 #========================================================================
 #Setting up regular expressions
@@ -25,70 +23,8 @@ from zxcvbn import zxcvbn
 #I had to split the negative look-behinds (?<!https) into multiple because python does not seems to support them together (eg : (?<!https|http))
 idSemicolumnThenItem = regex.compile(r'\b[^:^\n0-9]+(?<!http)(?<!https)(?<!ftp)(?<!sftp)(?<!rtmp)(?<!ws):([^\s\n]+)') #TODO: check this
 
-
-
-
-
-########################################## FUNCTONS ##########################################
-
-#Get the repo name from the URL (by trimming)
-def get_repo_name_from_url(url):
-    last_slash_index = url.rfind("/")
-    last_suffix_index = url.rfind(".git")
-    if last_suffix_index < 0:
-        last_suffix_index = len(url)
-
-    if last_slash_index < 0 or last_suffix_index <= last_slash_index:
-        raise Exception("Badly formatted url {}".format(url))
-
-    return url[last_slash_index + 1:last_suffix_index]
-
 #========================================================================
-
 #MODULE DELCARATION AND CONFIGURATION SECTION
-
-def parser_pastebin(url):
-    
-    tmp=tempfile.TemporaryFile(mode='r+')
-    #fetch url and write content in temporary file
-    tmp.write(requests.get(url).text)
-    print("Compiled one pastebin file")
-    return [tmp]
-
-def parser_github(url):
-    
-    returns=[]
-    #get repo name
-    reponame=get_repo_name_from_url(url)
-    repopath=os.path.join(os.getcwd(), 'repos/'+reponame)
-    #clone the repo
-    if os.path.isdir(repopath):
-        repo = Repo(repopath)
-        pullinfo = repo.remotes.origin.pull()
-    #    pullchanges=repo.index.diff(pullinfo[0].old_commit) #TODO: check this
-
-    else:
-        repo = Repo.clone_from(url, repopath)
-    #find all files in the repo
-    for root, subdirs, files in os.walk(repo.working_tree_dir):
-        for file in files:
-            filepath=os.path.join(root, file)
-            if mime.from_file(filepath) == "text/plain": #only allow plain text files
-                #with open(filepath, "rb") as readfile:
-                    #append their content to the temporary file
-                    #shutil.copyfileobj(readfile, tmp)
-                returns.append(filepath)
-        
-    #send the temporary file file descriptor
-
-    return returns
-
-#add your crawler in this dict to register it, the key is the one the DB should use
-parsers = {
-  "pastebin": parser_pastebin,
-  "git": parser_github,
-  "github": parser_github
-}
 
 #========================================================================
 
@@ -96,7 +32,7 @@ parsers = {
 #Parses a given file and then extracts all passwords and hashes founded. Return a mutli-level dict
 def analyzeFile(fileDscrpt, srckey):
     global idSemicolumnThenItem, hashID
-    print("Processing one file")
+    
 
     #return dict structure
     results = {
@@ -124,11 +60,11 @@ def analyzeFile(fileDscrpt, srckey):
     print("Method: ["+method+"]")
 
     for line in fileDscrpt:  #we parse line by line
-        print("Ligne: "+line)
+        #print("Ligne: "+line)
         if method == "semicolonlist":
             d = idSemicolumnThenItem.match(line) #apply the regex
-            print("Applied regex")
-            print(d)
+            #print("Applied regex")
+            #print(d)
 
             if lines < 1000: #we don't really care about counting lines if we are over 1000, this is only used to detect if this is a list or not, and only on first lines
                 lines += 1
@@ -139,14 +75,14 @@ def analyzeFile(fileDscrpt, srckey):
 
         if method == "fulllist" or d: #if the regex match or we are in fulllist mode (= each line is a "string of interest")
             matches += 1 
-            print("Got One match")
+            #print("Got One match")
             if method == "semicolonlist":
                 item=d.group(1).lstrip() #strip leading spaces if there were some after the semicolon
             else:
                 item=line # the whole line is kept in fulllist mode
             item=item.rstrip('\r\n') #remove trailing newlines (works for any combinaisons, so it works for DOS, OSX and Unix line endings)
             #print(item)
-            print("Checking for hash")
+            #print("Checking for hash")
             hashType = hashID.identifyHash(item)  #try to identify a hash
             hashcatcount=0 #number of possible hash types identified
             #hash data structure declaration, this is what is sent via rabbitMQ. This is stored in the return dict in an array of these
@@ -158,7 +94,7 @@ def analyzeFile(fileDscrpt, srckey):
             #for each identified modes, we store them in the struct        
             for mode in hashType:
                 if mode.hashcat is not None: #we only keep hashes hashcat can break, it should also filter out obscure hashes that got matched by a hash regex (by hashid)
-                    print(mode.name)
+                    #print(mode.name)
                     #print(mode.hashcat)
                     hashSummary["possibleHashTypes"].append(mode.hashcat)
                     hashSummary["possibleHashTypesNames"].append(mode.name)
@@ -167,8 +103,8 @@ def analyzeFile(fileDscrpt, srckey):
             #if there are no match, it must me plain text            
             isPassword=False
             if hashcatcount > 0: 
-                print("Got a hash")
-                print("Testing for false positive")                  #TODO: find a better way because we're going to have a lot of false positive.'
+                #print("Got a hash")
+                #print("Testing for false positive")                  #TODO: find a better way because we're going to have a lot of false positive.'
                 results = zxcvbn(item)
                 if results['guesses_log10'] >= 11:
                     hashSummary["value"]=item
@@ -192,40 +128,20 @@ def callback(ch, method, properties, body):
     print(" ") #For log clarity 
     print("processing:")
     print(params["v"])
-    datafiles=parsers[params["m"]](params["v"])  #we select and execute the appropriate parser by name from the parser list. (the parser name is also in the packet)
+    #datafiles=parsers[params["m"]](params["v"])  #we select and execute the appropriate parser by name from the parser list. (the parser name is also in the packet)
     # we get a file descriptor we can pass to analyseFile, we get a dict which contains everything extracted from it
-    for datafile in datafiles:
-        srckey=params["v"]
-        if isinstance(datafile, str):
-            file=open(datafile, "r", encoding="utf-8", errors="ignore")
-            srckey+=":"+os.path.basename(datafile)
-        else:
-            file=datafile
-        analyzeFile(file, srckey)
-        file.close() #we close the file, VERY IMPORTANT
+    filename=os.path.join(tmpDirectory,params["v"])
+    #Latin 1 will work for utf-8 but may mangle character if we edit the stream (see the official doc)
+    with open(filename, 'r', encoding="latin-1") as file:
+        print("Processing file : "+filename)
+        analyzeFile(file, params["s"])
 
     print("LAST COMMIT")
     mariadb_connection.commit()
+    print("Deleting source file")
+    os.remove(filename)
     print("ACK-ING the message")
     ch.basic_ack(method.delivery_tag)
-    """
-    if "passwords" in data:
-        for password in data["passwords"]: #if we have passwords, then for each one do            
-            #DB insert
-            
-            print("[Saving] Password: "+password)
-            sql = "INSERT INTO dict (password, seen) VALUES (%s, 0) ON DUPLICATE KEY UPDATE seen=seen+1"  #insert the password, and if it already exist, increment the "seen" counter
-            val = [password]
-            try:
-                cursor.execute(sql, val)
-            except mariadb.DataError:
-                print("[Saving] [Error]")
-    if "hashes" in data:    
-        for ihash in data["hashes"]: #if we have hashes, then for each one do        
-            print("[Saving] Hash: "+ihash)
-            message=json.dumps(ihash) #serializing the dict to json
-            #send the message through rabbbitMQ using the hashes exchange
-            channel.basic_publish(exchange='hashes', routing_key='', body=message)"""
     
 #========================================================================
 
@@ -277,83 +193,85 @@ def sendHash(ihash):
 #========================================================================
 
 hashID = hashid.HashID()
-
 #========================================================================
 
-mime = magic.Magic(mime=True)
 ########################################## END FUNCTONS #######################################
 
+def main():
+    global mariadb_connection, dbactioncount, cursor, channel
+    #========================================================================
+    #Connecting to mariadb
+    success=False
+    while not success:
+        try:
+            mariadb_connection = mariadb.connect(host='db_dict', user=os.environ['MYSQL_USER'], password=os.environ['MYSQL_PASSWORD'], database='crack_it')
+            success=True
+        except mariadb._exceptions.OperationalError as e:
+            success=False
+            print("Failed to connect to database... Retrying in 5 seconds.")
+            time.sleep(5)
 
-#========================================================================
-#Connecting to mariadb
-success=False
-while not success:
-    try:
-        mariadb_connection = mariadb.connect(host='db_dict', user=os.environ['MYSQL_USER'], password=os.environ['MYSQL_PASSWORD'], database='crack_it')
-        success=True
-    except mariadb._exceptions.OperationalError as e:
-        success=False
-        print("Failed to connect to database... Retrying in 5 seconds.")
-        time.sleep(5)
-
-#QUERY INIT
-cursor = mariadb_connection.cursor()
-
-
-#========================================================================
-#Connecting to RabbitMQ
-success=False
-while not success:
-    try:
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
-        channel = connection.channel()
-        #channel_uplink = connection.channel()
-        channel.basic_qos(prefetch_count=1)
-        #channel_uplink.basic_qos(prefetch_count=1)
-        success=True
-    except (pika.exceptions.AMQPConnectionError) as e:
-        success=False
-        print("Failed to connect to rabbitMQ ... Retrying in 5 seconds.")
-        time.sleep(5)
+    #QUERY INIT
+    cursor = mariadb_connection.cursor()
 
 
-#========================================================================
-#Setting up queues
-
-#Create the exchanges if they do not exist already
-channel.exchange_declare(exchange='urls', exchange_type='fanout')  #to get url to parse
-#channel.exchange_declare(exchange='hashes', exchange_type='fanout') #to send all hashes to cracker instances
-
-#announce we have a queue because we are a data consumer
-queue_name = 'parser_urls_queue'
-result = channel.queue_declare(queue=queue_name, exclusive=False, auto_delete=False)
-#bind the queue to the url exchange
-channel.queue_bind(exchange='urls', queue=queue_name)
-
-
-#queue_name2 = 'cracker_hashes_queue'
-#result2 = channel.queue_declare(queue=queue_name2, exclusive=False, auto_delete=False)
-#bind the queue to the url exchange
-#channel.queue_bind(exchange='hashes', queue=queue_name2)
-
-#========================================================================
-
-#Global variable useb in multiple functions - DO NOT TOUCH
-#Count actions on db before commiting
-dbactioncount=0
-
-#========================================================================
+    #========================================================================
+    #Connecting to RabbitMQ
+    success=False
+    while not success:
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+            channel = connection.channel()
+            #channel_uplink = connection.channel()
+            channel.basic_qos(prefetch_count=1)
+            #channel_uplink.basic_qos(prefetch_count=1)
+            success=True
+        except (pika.exceptions.AMQPConnectionError) as e:
+            success=False
+            print("Failed to connect to rabbitMQ ... Retrying in 5 seconds.")
+            time.sleep(5)
 
 
-######################################### START #########################################
+    #========================================================================
+    #Setting up queues
+
+    #Create the exchanges if they do not exist already
+    channel.exchange_declare(exchange='files', exchange_type='fanout')  #to get url to parse
+    #channel.exchange_declare(exchange='hashes', exchange_type='fanout') #to send all hashes to cracker instances
+
+    #announce we have a queue because we are a data consumer
+    queue_name = 'parser_files_queue'
+    result = channel.queue_declare(queue=queue_name, exclusive=False, auto_delete=False)
+    #bind the queue to the url exchange
+    channel.queue_bind(exchange='files', queue=queue_name)
 
 
-channel.basic_consume(queue_name, callback, auto_ack=False) #registering processing function
+    #queue_name2 = 'cracker_hashes_queue'
+    #result2 = channel.queue_declare(queue=queue_name2, exclusive=False, auto_delete=False)
+    #bind the queue to the url exchange
+    #channel.queue_bind(exchange='hashes', queue=queue_name2)
 
-channel.start_consuming() #start processing messages in the url queue
+    #========================================================================
+
+    #Global variable useb in multiple functions - DO NOT TOUCH
+    #Count actions on db before commiting
+    dbactioncount=0
+
+    #========================================================================
+
+
+    ######################################### START #########################################
+
+
+    channel.basic_consume(queue_name, callback, auto_ack=False) #registering processing function
+
+    channel.start_consuming() #start processing messages in the url queue
 
 
 
-#========================================================================
-#closing connection to rabbitMQ, important 
-connection.close()
+    #========================================================================
+    #closing connection to rabbitMQ, important 
+    connection.close()
+
+if __name__ == '__main__':
+    main()
